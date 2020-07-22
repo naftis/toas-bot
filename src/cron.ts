4002f6c9aedc94af1dfd233e65ffa3fc41e8bf68
naftis <pyry.rouvila@gmail.com>
2020-07-22 15:54:10 +0300

    Change cron package

--- src/cron.ts
+++ src/cron.ts
@@ -1,15 +1,14 @@
-import { CronJob } from "cron";
+import cron from "node-cron";
 import * as bot from "./bot";
 import * as api from "./api";
 
 export function start() {
-  const job = new CronJob("0 0 12,20 * * *", async () => {
+  const job = cron.schedule("0 0 12,20 * * *", async () => {
     const now = new Date();
     console.info(now.toUTCString(), `Running cron job...`);
     const chatIds = await api.getChatIds();
     await bot.sendTOASQueues(chatIds);
   });
-  job.start();
 
   console.info(`Cronjob initalized.`);
 }
